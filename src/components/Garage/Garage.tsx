3720797d4823ed9b17ea6e918c8fa2c9dcf418a7
Sevak Khnkoyan <sevak.khnkoyan@digitain.com>
2025-09-09 02:36:14 +0400

    feat: implement empty page behavior

--- src/components/Garage/Garage.tsx
+++ src/components/Garage/Garage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Garage.scss';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import {
@@ -10,7 +10,7 @@ import { Loader } from '../Loader/Loader';
 import { CarIcon } from './CarIcon';
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
-import { selectCarForEditing } from '../../store/garageSlice';
+import { goToPage, selectCarForEditing } from '../../store/garageSlice';
 import { ControlPanel } from '../ControlPanel/ControlPanel';
 import { PAGE_SIZE } from '../../constants';
 
@@ -23,6 +23,13 @@ const Garage: React.FC = () => {
   const [createCar, { isLoading: isCreating }] = useCreateCarMutation();
   const [deleteCar] = useDeleteCarMutation();
 
+  // If current page becomes empty but there are cars overall, go back one page
+  useEffect(() => {
+    if (!isFetching && page > 1 && total > 0 && cars.length === 0) {
+      dispatch(goToPage(page - 1));
+    }
+  }, [cars.length, total, page, isFetching, dispatch]);
+
   return (
     <>
       {(isCreating || isFetching) && <Loader />}
@@ -32,36 +39,46 @@ const Garage: React.FC = () => {
         <ControlPanel onCreate={createCar} />
 
         <div className="cars-list">
-          {cars.map((car) => (
-            <div className="car-item" key={car.id}>
-              <div className="car-item-buttons">
-                <div className="car-item-buttons__edit">
-                  <button
-                    className="garage-button small purple"
-                    onClick={() => dispatch(selectCarForEditing(car.id))}
-                  >
-                    Select
-                  </button>
-                  <button className="garage-button small red" onClick={() => deleteCar(car.id)}>
-                    Delete
-                  </button>
-                </div>
-                <div className="car-item-buttons__engine">
-                  <button className="garage-button small green" onClick={() => console.log(car.id)}>
-                    Start
-                  </button>
-                  <button className="garage-button small red" onClick={() => console.log(car.id)}>
-                    Stop
-                  </button>
-                </div>
-              </div>
-              <CarIcon color={car.color} />
-              <span className="garage-text">{car.name}</span>
-              <div className="garage-text">
-                _____________________________________________________________________________________________________________________________________________________________________________________________
-              </div>
+          {!isFetching && total === 0 ? (
+            <div className="garage-empty">
+              <p className="garage-empty__title">No Cars</p>
+              <p className="garage-empty__hint">Use the form above to create your first car.</p>
             </div>
-          ))}
+          ) : (
+            cars.map((car) => (
+              <div className="car-item" key={car.id}>
+                <div className="car-item-buttons">
+                  <div className="car-item-buttons__edit">
+                    <button
+                      className="garage-button small purple"
+                      onClick={() => dispatch(selectCarForEditing(car.id))}
+                    >
+                      Select
+                    </button>
+                    <button className="garage-button small red" onClick={() => deleteCar(car.id)}>
+                      Delete
+                    </button>
+                  </div>
+                  <div className="car-item-buttons__engine">
+                    <button
+                      className="garage-button small green"
+                      onClick={() => console.log(car.id)}
+                    >
+                      Start
+                    </button>
+                    <button className="garage-button small red" onClick={() => console.log(car.id)}>
+                      Stop
+                    </button>
+                  </div>
+                </div>
+                <CarIcon color={car.color} />
+                <span className="garage-text">{car.name}</span>
+                <div className="garage-text">
+                  _____________________________________________________________________________________________________________________________________________________________________________________________
+                </div>
+              </div>
+            ))
+          )}
         </div>
 
         <Footer page={page} total={total} limit={PAGE_SIZE} />
